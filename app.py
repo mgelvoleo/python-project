30b4c4d1c9e445ef2634b366e197278b378a3164
Mark Gelvoleo <mgelvoleo@gmailcom>
2026-01-02 17:49:14 +0800

    add stage for cleaning up the images local and online

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins CI/CD! testing 12"
+    return "Hello from Jenkins CI/CD! testing 13"
 
 
 if __name__ == "__main__":
