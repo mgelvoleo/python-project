18b935007a027035616ac85439d5316405fa3184
Mark Gelvoleo <mgelvoleo@gmailcom>
2026-01-09 18:51:57 +0800

    add change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins CI/CD! testing 19"
+    return "Hello from Jenkins CI/CD! testing 20"
 
 
 if __name__ == "__main__":
