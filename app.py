7321aa24b5a432d2ad9f426b526c6d651cfc163c
Mark Gelvoleo <mgelvoleo@gmailcom>
2026-01-09 21:20:18 +0800

    trigger jenkins

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins CI/CD! testing dev"
+    return "Hello from Jenkins CI/CD! testing dev 1"
 
 
 if __name__ == "__main__":
