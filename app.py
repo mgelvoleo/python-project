5dba95155dacb0454b87aef5cc13418094c9fdc0
Mark Gelvoleo <mgelvoleo@gmailcom>
2026-01-24 13:43:43 +0800

    test push

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Jenkins CI/CD! testing dev 3"
+    return "Hello from Jenkins CI/CD! testing dev 4"
 
 
 if __name__ == "__main__":
